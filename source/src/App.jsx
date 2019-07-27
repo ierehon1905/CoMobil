89435327e2a4bd204db948d8ce2ff19fc6ee9828
ilya-zubkov <ilya-zubkov@yandex-team.ru>
2019-07-27 14:51:05 +0300

    add router

--- source/src/App.jsx
+++ source/src/App.jsx
@@ -1,6 +1,39 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import { Input, Button, Icon, Typography, Row, Col } from 'antd';
+import {
+  Input, Button, Icon, Typography, Row, Col,
+} from 'antd';
+
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Map from './components/Map';
+
+
+const Kek = () => (
+  <React.Fragment>
+      <Row>
+      <Col span={6}>
+        <Icon type="bars" />
+      </Col>
+      <Col span={12}>
+        <Title>КАРэта</Title>
+      </Col>
+        {/* <Button type="default">|||</Button> */}
+        <Col span={6}>
+          <Icon type="user" />
+        </Col>
+      </Row>
+
+
+    <div>
+    <div>
+      <Input placeholder="from" />
+      <Input placeholder="to" />
+    </div>
+    </div>
+  </React.Fragment>
+);
 
 
 const { Title } = Typography;
@@ -8,26 +41,12 @@ const { Title } = Typography;
 function App() {
   return (
     <div className="App">
-      <Row>
-       <Col span={6}>
-        <Icon type="bars" />
-       </Col>
-       <Col span={12}>
-        <Title>КАРэта</Title>
-       </Col>
-        {/* <Button type="default">|||</Button> */}
-        <Col span={6}>
-          <Icon type="user" />
-        </Col>
-      </Row>
-      <div>
-        <div>
-          <Input placeholder="from" />
-          <Input placeholder="to" />
-        </div>
-      </div>
+       <Router>
+          <Route path="/" exact component={Kek} />
+          <Route path="/map-exp/" component={Map} />
+       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
